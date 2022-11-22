2324dbaadccf6691e2d3e9cf411babe7da945374
mvoagency <dev@mvo.agency>
2022-11-22 13:38:40 +0530

    22-11-2022

---
+++ src/main/java/com/imran/jpa/repository/DepartmentRepository.java
@@ -0,0 +1,9 @@
+package com.imran.jpa.repository;
+
+import com.imran.jpa.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+}
